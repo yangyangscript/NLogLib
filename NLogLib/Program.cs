069eae7f87478ae5cdd0d73c3efdbeeec71eeb71
yangyangWork <1324954289@qq.com>
2019-05-20 15:03:17 +0800

    添加项目文件。

---
+++ NLogLib/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            int i = 0;
+            while (i<10000)
+            {
+                Thread.Sleep(1000);
+                logger.Info($"日志测试:{i};");
+                i++;
+            }
+        }
+    }
+}
